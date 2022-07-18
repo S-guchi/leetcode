5922e30827917f6844cc6d6b3500de4881f173a8
shoya <cook.shoya@gmail.com>
2022-07-18 13:20:30 +0900

    1.,two-sum

--- 1.two-sum/1.two-sum.py
+++ 1.two-sum/1.two-sum.py
@@ -27,21 +27,7 @@ class Solution:
                     return [xi, yi]
 
 
-# https://youtu.be/KLlXCFG5TnA
-# 解説に載っていたコード
-# ハッシュを使う
-# targetからvalueを引きdiff配列に入れて、
-# if diff in prevMap:で　prevMapをチェックして、valueが含まれていたら
-# 足した数がtargetになる要素が見つかったってことになる
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         prevMap = {}  # val:index
-#         for i, n in enumerate(nums):
-#             diff = target - n
-#             if diff in prevMap:
-#                 return [prevMap[diff], i]
-#             prevMap[n] = i
-#         return
+
 
 # @lc code=end
 
