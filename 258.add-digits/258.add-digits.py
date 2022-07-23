b196afcfc51d850a2378ca59606d9bed5d076351
shoya <cook.shoya@gmail.com>
2022-07-23 10:03:11 +0900

    258.add-digits

--- 258.add-digits/258.add-digits.py
+++ 258.add-digits/258.add-digits.py
@@ -10,18 +10,24 @@ from unittest import result
 
 class Solution:
     def addDigits(self, num: int) -> int:
-        ary = []
-        while True:
-            result = 0
-            for i in range(len(str(num))):
-                num, surplus = divmod(num, 10)
-                result = result + surplus
+        # ary = []
+        # while True:
+        #     result = 0
+        #     for i in range(len(str(num))):
+        #         num, surplus = divmod(num, 10)
+        #         result = result + surplus
 
-            if result >= 10:
-                num = result
-            else:
-                break
-        return result
+        #     if result >= 10:
+        #         num = result
+        #     else:
+        #         break
+        # return result
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        else:
+            return num % 9
 
 
 # @lc code=end
